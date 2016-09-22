3c93f1fc105de18d744adaa1f519eb0f1dacd0bb
Start Coding Student 2 <session2@START-Coding-MBP-25.local>
2016-09-22 16:59:30 -0700

    Added a admin class extends from the account class

--- Admin.php
+++ Admin.php
@@ -2,8 +2,32 @@
 
 class Admin extends Account{
 
-  
+  private function dang(){
+
+    echo "<h1>I am the admin</h1";
+
+  }
+
+  public function addQuote(){
+
+    echo " I am ".$this->username." and this is the admin <br>";
+
+  }
 
 }
 
  ?>
+
+
+class intro{
+
+  public static $name;
+
+  public function sayHello($name){
+
+    echo 'Hello All, I am '.$name ;
+
+  }
+
+
+}
